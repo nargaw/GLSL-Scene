0c4f75c11fbf6bf92b9adcf589045e53bf3d81fc
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2022-08-25 17:06:31 -0400

    new proj

---
+++ src/Experience/World/Tv.js
@@ -0,0 +1,46 @@
+import * as THREE from 'three'
+import Experience from '../Experience.js'
+
+export default class Fox
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        this.time = this.experience.time
+        this.debug = this.experience.debug
+
+        // Debug
+        if(this.debug.active)
+        {
+            this.debugFolder = this.debug.ui.addFolder('TV')
+        }
+
+        // Resource
+        this.resource = this.resources.items.tvModel
+
+        this.setModel()
+        
+    }
+
+    setModel()
+    {
+        this.model = this.resource.scene
+        //this.model.scale.set(0.02, 0.02, 0.02)
+        this.scene.add(this.model)
+
+        this.model.traverse((child) =>
+        {
+            if(child instanceof THREE.Mesh)
+            {
+                child.castShadow = true
+            }
+        })
+    }
+
+    update()
+    {
+        
+    }
+}
